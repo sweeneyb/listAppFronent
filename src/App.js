40a79cd398df95fd429aa6d7d755a0e3c2b1dfab
Ubuntu <ubuntu@ubuntu-xenial.localdomain>
2018-05-06 13:41:31 +0000

    proxy and some fetching

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import logo from '../logo.svg';
+import '../App.css';
 
 class App extends Component {
   render() {
@@ -11,11 +11,31 @@ class App extends Component {
           <h1 className="App-title">Welcome to React</h1>
         </header>
         <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
+          To get started, edit <code>src/App.js</code> and save to reload.  Woo!
         </p>
       </div>
     );
   }
+
+  componentWillMount() {
+    console.log("in will mount")
+    fetch("/listItems")
+    .then(handleErrors)
+    .then(res => {return res.json})
+    .then(res => console.log(res) )
+    // .then(res => console.log(res['body']))
+    // .then(res => console.log(res.body))
+    console.log("done fetching")
+  }
+
+  
+}
+// Handle HTTP errors since fetch won't.
+function handleErrors(response) {
+  if (!response.ok) {
+     throw Error(response.statusText);
+  }
+  return response;
 }
 
 export default App;
